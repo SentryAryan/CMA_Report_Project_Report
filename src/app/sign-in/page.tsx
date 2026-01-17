e0ee70a5f3ef7b4fc982d08d45ba1cb31413033a
Anikrawat <anikrawat18@gmail.com>
2026-01-17 12:14:37 +0530

    Setup

---
+++ src/app/sign-in/page.tsx
@@ -0,0 +1,7 @@
+export default function SignIn() {
+  return (
+    <div className="flex min-h-screen items-center justify-center font-sans ">
+      Hello thits is sign in page
+    </div>
+  );
+}
